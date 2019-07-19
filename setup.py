f0a4a63896455ee69ef904339c0cebd1fc77156f
Sophia <sophia.sanborn@gmail.com>
2019-07-18 17:39:17 -0700

    updated setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='rlbase',
+    version='0.0.1',
+    description='Deep RL infrastructure',
+    url='git@github.com:sophiaas/rlbase.git',
+    author='Sophia Sanborn',
+    author_email='sanborn@berkeley.edu',
+    license='unlicense',
+    packages=['rlbase'],
+    zip_safe=False
+)
