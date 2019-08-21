917e9f4a0028bd57ed670a9851237d851cd6eddd
EC2 Default User <ec2-user@ip-172-31-0-86.us-east-2.compute.internal>
2019-08-21 06:28:34 +0000

    fix

--- rlbase/gridsearch_cross.py
+++ rlbase/gridsearch_cross.py
@@ -10,8 +10,6 @@ parser = argparse.ArgumentParser(description='Lightbot')
 
 parser.add_argument('--config', type=str, default='lightbot_cross',
                     help='Name of config') 
-parser.add_argument('--algo', type=str, default='ppo',
-                    help='Algorithm') 
 parser.add_argument('--name', type=str, default=None,
                     help='Name to prepend to save dir') 
 parser.add_argument('--device', type=int, default=None,
@@ -19,20 +17,11 @@ parser.add_argument('--device', type=int, default=None,
 
 args = parser.parse_args()
 
-if args.algo == 'ppo':
-    from configs.ppo import all_configs
-    agent = PPO
-
-elif args.algo == 'ppoc':
-    from configs.ppoc import all_configs
-    agent = PPOC
-
-else:
-    raise ValueError('Specified algorithm is not yet implemented')
-    
+from configs.ppo import all_configs
+agent = PPO
     
 
-def get_chunks(iterable, chunks=12):
+def get_chunks(iterable, chunks=14):
     lst = list(iterable)
     return [lst[i::chunks] for i in range(chunks)]
 
@@ -69,6 +58,7 @@ if __name__ == '__main__':
     jobs = []
     grid = itertools.product(lrs, lr_gammas, rl_gammas, taus, lr_step_intervals, ppo_clips)
     chunked_args = get_chunks(grid, chunks=12)
+    chunked_args = [x[1:] for x in chunked_args] # To avoid re-running
     pool = multiprocessing.Pool()
     pool.map(worker, chunked_args)
     pool.close()
