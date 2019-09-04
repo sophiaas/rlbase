e21ef6fbb4eee6ea0b743213cfd37976a561bdb5
Sophia <sophia.sanborn@gmail.com>
2019-09-04 23:43:57 +0000

    removes unnecessary label

--- rlbase/train.py
+++ rlbase/train.py
@@ -3,7 +3,7 @@ import argparse
 from agents import PPO, PPOC
 import torch
 
-parser = argparse.ArgumentParser(description='Lightbot')
+parser = argparse.ArgumentParser()
 
 parser.add_argument('--config', type=str, default='lightbot_cross',
                     help='Name of config') 
