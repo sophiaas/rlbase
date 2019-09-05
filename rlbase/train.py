64971c86c42153ddd7ebcb3974a8ab77cd0cf6bb
Michael Chang <mbchang2017@gmail.com>
2019-09-05 12:00:14 -0700

    puzzle --> args.puzzle

--- rlbase/train.py
+++ rlbase/train.py
@@ -37,7 +37,7 @@ if args.name is not None:
     config.experiment.name = args.name + '_' + config.experiment.name
     
 if args.puzzle:
-    config.env.puzzle_name = puzzle
+    config.env.puzzle_name = args.puzzle
     config.experiment.name = config.experiment.name + '_' + args.puzzle
     
 if args.n_disks:
