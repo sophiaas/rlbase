2f815003b39d690784cb46499487152bde06ffb4
Michael Chang <mbchang2017@gmail.com>
2019-09-08 11:22:22 -0700

    runner eval

---
+++ runner_eval.py
@@ -0,0 +1,64 @@
+import os
+import itertools
+
+"""
+# Hanoi
+
+'python rlbase/train.py --algo ppo --config hanoi --n_disks 2'
+'python rlbase/train.py --algo ppo --config hanoi --n_disks 3'
+'python rlbase/train.py --algo ppo --config hanoi --n_disks 4'
+
+'python rlbase/train.py --algo ppoc --config hanoi --n_disks 2'
+'python rlbase/train.py --algo ppoc --config hanoi --n_disks 3'
+'python rlbase/train.py --algo ppoc --config hanoi --n_disks 4'
+
+# lightbot minigrid
+
+'python rlbase/train.py --algo ppo --config lightbot_minigrid --puzzle fractal_cross_0'
+'python rlbase/train.py --algo ppo --config lightbot_minigrid --puzzle fractal_cross_1'
+'python rlbase/train.py --algo ppo --config lightbot_minigrid --puzzle fractal_cross_2'
+
+'python rlbase/train.py --algo ppoc --config lightbot_minigrid --puzzle fractal_cross_0'
+'python rlbase/train.py --algo ppoc --config lightbot_minigrid --puzzle fractal_cross_1'
+'python rlbase/train.py --algo ppoc --config lightbot_minigrid --puzzle fractal_cross_2'
+
+# lightbot
+
+'python rlbase/train.py --algo ppo --config lightbot'
+'python rlbase/train.py --algo ppoc --config lightbot'
+
+# fourrooms
+'python rlbase/train.py --algo ppo --config fourrooms'
+'python rlbase/train.py --algo ppoc --config fourrooms'
+"""
+
+prefix ='python rlbase/evaluate.py --model_dir'
+
+model_dirs = [
+    # lightbot state space
+    '/home/mbchang/shared/ssc/rlbase/experiments/adam_ppo_lightbot_lr0.0001',
+    '/home/mbchang/shared/ssc/rlbase/experiments/adam_ppoc_lightbot_lr0.0005',
+
+    # fourrooms
+    '/home/mbchang/shared/ssc/rlbase/experiments/betterlr_ss/betterlr_ss_ppo_fourooms_lr0.001',
+    '/home/mbchang/shared/ssc/rlbase/experiments/betterlr_ss/betterlr_ss_ppoc_fourrooms_lr0.001',
+
+    # hanoi
+
+    # lightbot minigrid
+    '/home/mbchang/shared/ssc/rlbase/experiments/sparse500000/sparse500000_ppo_lightbot_minigrid_fractal_cross_0_lr0.001',
+    '/home/mbchang/shared/ssc/rlbase/experiments/sparse500000/sparse500000_ppoc_lightbot_minigrid_fractal_cross_0_lr0.001',
+]
+
+num_gpus = 2
+i = 0
+
+for model_dir in model_dirs:
+    logfile = os.path.join(model_dir, 'eval.txt')
+    command = 'CUDA_VISIBLE_DEVICES={} {} {} > {} &'.format(i, prefix, model_dir, logfile)
+    print(command)
+    # os.system(command)
+
+    i += 1
+    if i >= num_gpus:
+        i = 0
