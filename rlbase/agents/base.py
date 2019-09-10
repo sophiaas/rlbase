5668c3148fd28a96342a07a2934acb764a80adbf
Sophia <sophia.sanborn@gmail.com>
2019-09-10 02:27:54 +0000

    changes logging and plotting to per timestep

--- rlbase/agents/base.py
+++ rlbase/agents/base.py
@@ -46,16 +46,16 @@ class BaseAgent(object):
         else:
             return x
 
-    def get_summary(self):
-        summary = {
-            'episode': int(self.episode)-10,
-            'running_rewards': self.running_rewards,
-            'running_moves': self.running_moves
-            }
-        return summary
+#     def get_summary(self):
+#         summary = {
+#             'episode': int(self.episode)-50,
+#             'running_rewards': self.running_rewards,
+#             'running_moves': self.running_moves
+#             }
+#         return summary
             
-    def update_running(self, episode_rewards, episode_moves):
-        if self.episode == 10:
+    def update_running(self, episode_rewards, episode_moves, timestep):
+        if timestep == 50:
             self.running_rewards = episode_rewards   
             self.running_moves = episode_moves
         else:
@@ -68,77 +68,83 @@ class BaseAgent(object):
         avg_length = 0
         timestep = 0
         self.episode_steps = 0
+        
+        episode_reward = 0
+        episode_data = defaultdict(list, {'episode': int(self.episode)})
+        state = self.env.reset()
 
         # Iterate through episodes
-        for i_episode in range(1, self.config.training.max_episodes+1):
-            episode_reward = 0
-            episode_data = defaultdict(list, {'episode': int(self.episode)})
-            state = self.env.reset()
+        print('Max Timesteps: {}'.format(self.config.training.max_timesteps))
+        for timestep in range(1, self.config.training.max_timesteps+1):
+            
+#         for i_episode in range(1, self.config.training.max_episodes+1):
+
             
             # Iterate through steps
-            for t in range(1, self.config.training.max_episode_length+1):
-                timestep += 1
-                self.episode_steps += 1
-                state = torch.from_numpy(state).float().to(self.device)
-                
-                with torch.no_grad():
-                    transition, state, done = self.step(state)
-                
-                for key, val in transition.items():
-                    episode_data[key].append(self.convert_data(val))
-                
-                episode_reward += transition['reward']
-                running_reward += transition['reward']
-                
-                if self.config.experiment.render:
-                    self.env.render()
-                    
-                if done:
-                    break
+#             for t in range(1, self.config.training.max_episode_length+1):
+#                 timestep += 1
+            self.episode_steps += 1
+            state = torch.from_numpy(state).float().to(self.device)
 
-                if timestep % self.config.training.update_every == 0:
-                    self.update()
-                    self.memory.clear()
-            
+            with torch.no_grad():
+                transition, state, done = self.step(state)
+                
+            for key, val in transition.items():
+                episode_data[key].append(self.convert_data(val))
+
+            episode_reward += transition['reward']
+            running_reward += transition['reward']
+
+            if self.config.experiment.render:
+                self.env.render()
+                    
             if self.config.experiment.save_episode_data and self.episode % self.config.experiment.every_n_episodes == 0:
                 self.logger.push_episode_data(episode_data)
                 
-                
-            # Logging
-            if i_episode % self.config.experiment.log_interval == 0:
-                
-                print('Episode {} \t avg length: {} \t reward: {}'.format(i_episode, 
-                                                                    round(self.running_moves, 2), 
-                                                                    round(self.running_rewards, 2)))
-                self.logger.save()
-                self.logger.save_checkpoint(self)
-                
-                if self.config.experiment.save_episode_data:
-                    self.logger.save_episode_data()
-                
-                self.logger.plot('running_rewards')
-                self.logger.plot('running_moves')
-                
-            avg_length += self.episode_steps
-            
-#             if i_episode % 10 == 0:
-#                 self.update_running(running_reward/10, avg_length/10)
-#                 running_reward = 0
-#                 avg_length = 0
-#                 self.logger.push(self.get_summary())
-            if i_episode % 10 == 0:
-                self.update_running(running_reward/10, avg_length/10)
-                running_reward = 0
-                avg_length = 0
+            if done:
+                episode_data = defaultdict(list, {'episode': int(self.episode)})
                 summary = {
-                    'steps': timestep
-                    'return': episode_reward
+                    'steps': timestep,
+                    'return': episode_reward,
                     'moves': self.episode_steps
                 }
                 self.logger.push(summary)
+                state = self.env.reset()
                 
-            self.episode_steps = 0
-            self.episode += 1
+                # Logging
+                if self.episode % self.config.experiment.log_interval == 0:
+                    
+                    print('Episode {} \t avg length: {} \t reward: {}'.format(self.episode, 
+                                                                        round(self.episode_steps, 2), 
+                                                                        round(episode_reward, 2)))
+
+                    self.logger.save()
+                    self.logger.save_checkpoint(self)
+
+                    if self.config.experiment.save_episode_data:
+                        self.logger.save_episode_data(self.episode)
+
+                    self.logger.plot('return')
+                    self.logger.plot('moves')
+                avg_length += self.episode_steps
+                self.episode += 1
+                self.episode_steps = 0
+                episode_reward = 0
+
+            if timestep % self.config.training.update_every == 0:
+                self.update()
+                self.memory.clear()
+                
+             
+
+                
+
+            if timestep % 50 == 0:
+                self.update_running(running_reward/50, avg_length/50, timestep)
+                running_reward = 0
+                avg_length = 0
+
+                
             
         print('Training complete')
         
