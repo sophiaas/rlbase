3c84eb2bca29613ca55379bec0250e649b77d00f
Sophia <sophia.sanborn@gmail.com>
2019-10-11 02:22:41 +0000

    new config file for hanoi sequence state space

---
+++ rlbase/configs/ppo/hanoi_lstm.py
@@ -0,0 +1,77 @@
+from core.config import *
+from torch.optim.lr_scheduler import StepLR
+from torch.optim import Adam
+from networks.heads import FullyConnectedHead
+from networks.bodies import LSTMBody
+
+HDIM = 256
+
+experiment = ExperimentConfig(
+    {'name': 'ppo_hanoi',
+    }
+)
+
+algorithm = PPOConfig(
+    {}
+)
+
+training = TrainingConfig(
+    {
+    # 'max_episode_length': 500,
+     # 'max_timesteps': 500000,
+    }
+)
+
+policy_head = FCConfig(
+    {'hdim': HDIM, 
+     'nlayers': 1,
+     'activation': nn.ReLU(),
+     'out_activation': nn.Softmax(dim=-1),
+     'architecture': FullyConnectedHead
+    }
+)
+
+value_head = FCConfig(
+    {'hdim': HDIM, 
+     'nlayers': 1,
+     'out_activation': None,
+     'architecture': FullyConnectedHead,
+     'outdim': 1
+    }
+)
+
+body = LSTMConfig(
+    {'hdim': HDIM, 
+     'nlayers': 2, 
+     'architecture': LSTMBody
+    }
+)
+
+network = NetworkConfig(
+    {'heads': {'actor': policy_head, 'critic': value_head},
+     'body': body
+    }
+)
+    
+env = HanoiConfig({})
+
+config = Config(
+    {'experiment': experiment, 
+     'algorithm': algorithm, 
+     'training': training, 
+     'network': network, 
+     'env': env
+    }
+)
+
+def post_process(config):
+    # post processing
+    if config.env.n_disks == 2:
+        print('000')
+    elif config.env.n_disks == 3:
+        print('111')
+    elif config.env.n_disks == 4:
+        print('222')
+    else:
+        assert False
+    return config
